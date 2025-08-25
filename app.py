47dc38b49932a192f12297ffa42871d3226394be
Arpit Dhote <95398246+arpitdhote51@users.noreply.github.com>
2025-08-25 22:58:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ load_dotenv(find_dotenv())
 from huggingface_hub import InferenceClient
 
 import os
-hf_token = os.getenv("HF_TOKEN")
+HF_token = os.getenv("HF_TOKEN")
+#HF_TOKEN = st.secrets["HUGGINGFACE_API_TOKEN"]
 
 
 
